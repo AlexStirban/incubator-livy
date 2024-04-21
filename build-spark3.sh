8d629426d150c0bcaaf3188cb5d82d3c8aea34a0
alexstirban <stirvaslex@gmail.com>
2024-04-21 18:09:29 +0200

    Added compression for distribution

--- build-spark3.sh
+++ build-spark3.sh
@@ -10,13 +10,21 @@ docker run --rm -it -v $(pwd):/workspace -v $HOME/.m2:/root/.m2 livy-ci mvn pack
 rm -rf dist
 
 # Make dirs target files
-mkdir -p dist/repl_2.12-jars
-mkdir -p dist/rsc-jars
-mkdir -p dist/jars
+mkdir -p dist/livy/repl_2.12-jars
+mkdir -p dist/livy/rsc-jars
+mkdir -p dist/livy/jars
 
 # Copy 
-cp -r bin dist/bin
-cp -r conf dist/conf
-cp server/target/jars/* dist/jars
-cp repl/scala-2.12/target/jars/* dist/repl_2.12-jars/
-cp rsc/target/jars/* dist/rsc-jars/
+cp -r bin dist/livy/bin
+cp -r conf dist/livy/conf
+cp server/target/jars/* dist/livy/jars
+cp repl/scala-2.12/target/jars/* dist/livy/repl_2.12-jars/
+cp rsc/target/jars/* dist/livy/rsc-jars/
+
+cp LICENSE dist/livy/LICENSE
+cp DISCLAIMER dist/livy/DISCLAIMER
+cp NOTICE dist/livy/NOTICE
+
+# Compress
+cd dist
+tar -czvf livy-0.8.0-spark3-jvm.tar.gz livy  
